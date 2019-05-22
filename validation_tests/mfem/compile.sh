0c29ea9889b19fc9541dbebda338b62f0344e27d
Wyatt Spear <wspear@cs.uoregon.edu>
2019-05-22 20:22:01 +0000

    Specify mfem location with spack call instead of hard-coding.

--- validation_tests/mfem/compile.sh
+++ validation_tests/mfem/compile.sh
@@ -2,5 +2,5 @@
 . ../../setup.sh
 spack load mfem 
 spack load mpich
-
+export MFEM_INSTALL_DIR=`spack location --install-dir mfem`
 make  
--- validation_tests/mfem/run.sh
+++ validation_tests/mfem/run.sh
@@ -2,6 +2,7 @@
 . ../../setup.sh
 spack load mfem
 spack load mpich 
+export MFEM_INSTALL_DIR=`spack location --install-dir mfem`
 
 mpirun -np 4 ./ex10p --mesh ./beam-quad.mesh
 
