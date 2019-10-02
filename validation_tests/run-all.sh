c17319cc1309a7f6e7349d1c94cac73a62274d98
eugeneswalker <eugenesunsetwalker@gmail.com>
2019-10-02 16:55:57 +0000

    add common run-all script

---
+++ validation_tests/run-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function e() {
+  ./"${1}" > /dev/null 2>&1
+  return $?
+}
+
+for d in "hypre" "sundials" "strumpack" "trilinos"
+do
+  passed=0
+  (cd "${d}" && e "clean.sh" && e "compile.sh" && e "run.sh") && passed=1
+  if [[ "${passed}" -eq 1 ]]; then
+    printf "${d} passed!\n"
+  else
+    printf "${d} FAILED**\n"
+  fi
+done
