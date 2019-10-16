dfbd40eac44f2f55be8c4a20f60e1850b71538bc
Wyatt Spear <wspear@cs.uoregon.edu>
2019-10-16 20:20:18 +0000

    Load and activate proper modules (including python3) for mpi4py example

--- validation_tests/mpi4py/run.sh
+++ validation_tests/mpi4py/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 . ../../setup.sh
-mpirun -np 4 python ./cpi.py
+spack activate py-mpi4py
+spack activate py-numpy
+spack load py-numpy
+spack load py-mpi4py
+spack load mpich
+spack load python@3:
+mpirun -np 4 python3 ./cpi.py
