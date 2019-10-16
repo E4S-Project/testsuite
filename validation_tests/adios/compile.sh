10af03096fbc282b9baa2cde6203d938bf554956
Wyatt Spear <wspear@cs.uoregon.edu>
2019-10-16 19:29:35 +0000

    Use local setup.sh to populate env for build and run. Modify makefile to apply lib variables

--- validation_tests/adios/compile.sh
+++ validation_tests/adios/compile.sh
@@ -1,11 +1,3 @@
 #!/bin/bash -e
-. ../../setup.sh
-spack load adios
-spack load lz4
-spack load zfp
-spack load sz
-spack load zlib
-spack load c-blosc
-spack load mpich
-
+. ./setup.sh
 make
--- validation_tests/adios/run.sh
+++ validation_tests/adios/run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
-. ../../setup.sh
-spack load adios
-spack load lz4
-spack load zfp
-spack load sz
-spack load zlib
-spack load c-blosc
-spack load mpich
-
-
+. ./setup.sh
 
 echo "Arrays Write Example"
 mpirun -np 8 ./arrays_write
