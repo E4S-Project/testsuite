1910413da597c102b2234f44eccbb7b58c7b5ea8
Wyatt Spear <wspear@cs.uoregon.edu>
2019-10-22 19:25:38 +0000

    Select and load one unique py-numpy

--- validation_tests/mpi4py/run.sh
+++ validation_tests/mpi4py/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+oneSpackHash(){ echo "/`spack find -l $1 | tail -n1 | awk '{print $1;}'`" ;  }
+PYNUMPY=`oneSpackHash py-numpy`
 . ../../setup.sh
 spack activate py-mpi4py
-spack activate py-numpy
-spack load py-numpy
+spack activate $PYNUMPY
+spack load $PYNUMPY
 spack load py-mpi4py
 spack load mpich
 spack load python@3:
