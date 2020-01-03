75d481e3741d211c33acc9554b894097bb1da205
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-01-03 15:47:03 -0800

    Typo in a script

--- validation_tests/llvm/f18/compile.sh
+++ validation_tests/llvm/f18/compile.sh
@@ -6,6 +6,6 @@ ulimit -s unlimited
 cd lapack && make blaslib lapacklib
 cd ..
 
-cd fgortran.dg && mkdir -f build && cd build && cmake .. -DFC=f18
+cd gfortran.dg && mkdir -f build && cd build && cmake .. -DFC=f18
 cd ../..
 
