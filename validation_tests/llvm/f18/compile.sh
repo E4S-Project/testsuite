089fcaf4a07b2f62e0adce93dcd70df14257ddea
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-01-03 16:13:12 -0800

    Fixed script

--- validation_tests/llvm/f18/compile.sh
+++ validation_tests/llvm/f18/compile.sh
@@ -6,6 +6,6 @@ ulimit -s unlimited
 cd lapack && make blaslib lapacklib
 cd ..
 
-cd gfortran.dg && mkdir -f build && cd build && cmake .. -DFC=f18
+cd gfortran.dg && if [ ! -d build ] ; then mkdir build ; fi && cd build && cmake .. -DFC=f18
 cd ../..
 
