69c908d08a893898fbf7c697b931096ac3b35e28
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-03-24 17:34:29 -0700

    Take the new line info into account

--- validation_tests/llvm/clacc/profiling/basic.c
+++ validation_tests/llvm/clacc/profiling/basic.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 void prof_inc(acc_prof_info* prof_info, acc_event_info* event_info,
               acc_api_info* api_info) {
-  printf("Event type %d file %s line %d func name %s acc version %d\n",
+  printf("Event type %d file %s line %d func name %s %d %d region %d %d\n",
          prof_info->event_type, prof_info->src_file, prof_info->line_no,
-         prof_info->func_name, prof_info->version);
+         prof_info->func_name, prof_info->func_line_no, prof_info->func_end_line_no, prof_info->line_no, prof_info->end_line_no );
+
+
+
 }
 void acc_register_library(acc_prof_reg reg, acc_prof_reg unreg,
                           acc_prof_lookup lookup) {
