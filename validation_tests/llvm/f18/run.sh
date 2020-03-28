d4580daee758bd320bfe2ac82e161d01e9fc50ca
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-03-28 01:52:34 -0700

    Run the tests with both backends

--- validation_tests/llvm/f18/run.sh
+++ validation_tests/llvm/f18/run.sh
@@ -2,8 +2,19 @@
 
 . ./setup.sh
 
-cd lapack && make blas_testing lapack_testing
+echo -e "\033[01;34m[Backend: gfortran]\e[0m"
+export F18_FC=gfortran
+
+cd lapack && make blas_testing lapack_testing TIMER=INT_ETIME
 cd ..
 cd gfortran.dg/build && ctest -j -R CMP && ctest -j -R EXE
+cd ..
+
+echo -e "\033[01;34m[Backend: clang]\e[0m"
+export F18_FC=clang
+
+cd lapack && make clean && make blas_testing lapack_testing
+cd ..
+cd gfortran.dg/build && ctest -j -R CMP && ctest -j -R EXE  TIMER=INT_CPU_TIME
 cd ..
 
