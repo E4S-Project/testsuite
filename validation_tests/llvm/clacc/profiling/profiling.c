4730b639247133453893bc17a0deda12531e4c9f
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-04-02 14:38:48 -0700

    Write more information in the profiling info mini-lib

--- validation_tests/llvm/clacc/profiling/profiling.c
+++ validation_tests/llvm/clacc/profiling/profiling.c
@@ -11,8 +11,52 @@
 unsigned int cnt = 0;
 #define VERSION 0.1
 
+typedef struct { int event; char name[256]; } my_event_t;
+
+my_event_t eventmap[] = {
+  { acc_ev_device_init_start, "acc_ev_device_init_start" },
+  { acc_ev_device_init_end, "acc_ev_device_init_end" },
+  { acc_ev_device_shutdown_start, "acc_ev_device_shutdown_start" },
+  { acc_ev_device_shutdown_end, "acc_ev_device_shutdown_end" },
+  { acc_ev_runtime_shutdown, "acc_ev_runtime_shutdown" }, 
+  { acc_ev_enter_data_start, "acc_ev_enter_data_start" },
+  { acc_ev_enter_data_end, "acc_ev_enter_data_end" },
+  { acc_ev_exit_data_start, "acc_ev_exit_data_start" },
+  { acc_ev_exit_data_end, "acc_ev_exit_data_end" },
+  { acc_ev_enqueue_launch_start, "acc_ev_enqueue_launch_start" },
+  { acc_ev_enqueue_launch_end, "acc_ev_enqueue_launch_end" },
+  { acc_ev_enqueue_upload_start, "acc_ev_enqueue_upload_start" },
+  { acc_ev_enqueue_upload_end, "acc_ev_enqueue_upload_end" },
+  { acc_ev_enqueue_download_start, "acc_ev_enqueue_download_start" },
+  { acc_ev_enqueue_download_end, "acc_ev_enqueue_download_end" },
+  { acc_ev_compute_construct_start, "acc_ev_compute_construct_start" },
+  { acc_ev_compute_construct_end, "acc_ev_compute_construct_end" },
+  { acc_ev_create, "acc_ev_create" },
+  { acc_ev_delete, "acc_ev_delete" },
+  { acc_ev_alloc, "acc_ev_alloc" },
+  { acc_ev_free, "acc_ev_free" },
+  { acc_ev_wait_start, "acc_ev_wait_start" },
+  { acc_ev_wait_end, "acc_ev_wait_end" },
+  { acc_ev_update_start, "acc_ev_update_start" },
+  { acc_ev_update_end, "acc_ev_update_end" },
+
+  //  { acc_ev_done, "acc_ev_done" },
+
+  {-1, ""},
+};
+
+/* Highly suboptimal, have C++ maps would help there */
+char* eventToName( int eventtype ){
+  int i = 0;
+  while( eventmap[i].event != -1 ) {
+    if( eventmap[i].event == eventtype ) return eventmap[i].name;
+    i++;
+  }
+  return "";
+}
+
 void prof_inc( acc_prof_info* prof_info, acc_event_info* event_info, acc_api_info* api_info ){
-  printf( "Event type %d  file %s line %d func name %s acc version %d\n", prof_info->event_type, prof_info->src_file, prof_info->line_no, prof_info->func_name, prof_info->version );
+  printf( "Event type %d (%s) file %s line %d func name %s acc version %d\n", prof_info->event_type, eventToName( prof_info->event_type), prof_info->src_file, prof_info->line_no, prof_info->func_name, prof_info->version );
 }
 
 void acc_register_library(acc_prof_reg reg, acc_prof_reg unreg,
