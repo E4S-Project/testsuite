7a1cb3c66dbc5ee392b1c8f5b490dda148f63000
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-05-27 15:39:39 -0700

    Fixed setup script to work with Spack

--- validation_tests/papyrus/compile.sh
+++ validation_tests/papyrus/compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export PAPYRUSDIR=/tmp/root/spack-stage/spack-stage-papyrus-1.0.0-j7sq7ppf24fu4q6mbgxoz24x434copzw/spack-src/install
-export LIBPAPYRUS=${PAPYRUSDIR}/lib/libpapyruskv.a
+. ./setup.sh
+export LIBPAPYRUS=${PAPYRUS_ROOT}/lib/libpapyruskv.a
 
 TESTLIST="01_open_close  02_put_get  03_barrier  04_delete  05_fence  06_signal  07_consistency  08_protect  09_cache  10_checkpoint  11_restart  12_free "
 # The last one, 13_upc, is using UPC.
 
 for d in $TESTLIST; do 
     cd $d
-    mpicxx -I${PAPYRUSDIR}/include -g -o test${d} test${d}.c $LIBPAPYRUS -lpthread -lm
+    mpicxx -I${PAPYRUS_ROOT}/include -g -o test${d} test${d}.c $LIBPAPYRUS -lpthread -lm
     cd ..
 done
--- validation_tests/papyrus/run.sh
+++ validation_tests/papyrus/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. ./setup.sh
 TESTLIST="01_open_close  02_put_get  03_barrier  04_delete  05_fence  06_signal  07_consistency  08_protect  09_cache  10_checkpoint  11_restart  12_free "
 NP=4
 
