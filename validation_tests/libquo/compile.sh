660e32367a8ecec24b101a238072bc66d7daa5f5
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-10-02 15:28:24 +0000

    libquo: use spackLoadUnique in setup; repair compile+run scripts

--- validation_tests/libquo/compile.sh
+++ validation_tests/libquo/compile.sh
@@ -8,10 +8,10 @@ FPROG="quofort.f90"
 
 for c in $CPROG; do
     n=`echo $c | sed "s/.c$//"`
-    mpicc -I${QUO_ROOT}/include -I${HWLOC_ROOT}/include $c -o $n -L${QUO_ROOT}/lib -L${HWLOC_ROOT}/lib -lquo -lhwloc -lm
+    mpicc -I${LIBQUO_ROOT}/include -I${HWLOC_ROOT}/include $c -o $n -L${LIBQUO_ROOT}/lib -L${HWLOC_ROOT}/lib -lquo -lhwloc -lm
 done
 
 for c in $FPROG; do
     n=`echo $c | sed "s/.f90$//"`
-    mpifort -I${QUO_ROOT}/lib -I${QUO_ROOT}/include -I${HWLOC_ROOT}/include $c -o $n -L${QUO_ROOT}/lib -L${HWLOC_ROOT}/lib -lquo -lquo-usequo -lhwloc -lm
+    mpifort -I${LIBQUO_ROOT}/lib -I${LIBQUO_ROOT}/include -I${HWLOC_ROOT}/include $c -o $n -L${LIBQUO_ROOT}/lib -L${HWLOC_ROOT}/lib -lquo -lquo-usequo -lhwloc -lm
 done
