ca8cb3c18a63f993ccee35aa777b1903d9ff7549
Frederick Deny <fdeny@uoregon.edu>
2020-10-13 10:47:44 -0700

    test done for tensorflow V1 and V2

--- validation_tests/tensorflow/tf.py
+++ validation_tests/tensorflow/tf.py
@@ -1,5 +1,5 @@
+from __future__ import print_function
 import sys
-
 #Call Check
 #========================================
 
@@ -30,14 +30,70 @@ import os
 #if sys.argv[2]=='NVIDIA':
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
+if sys.argv[3]=='1':
 
-## Initialize two constants
-#x1 = tf.constant([1,2,3,4])
-#x2 = tf.constant([5,6,7,8])
-#
-## Multiply
-#result = tf.multiply(x1, x2)
 
+    # Import MNIST data
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+    # Parameters
+    learning_rate = 0.1
+    num_steps = 500
+    batch_size = 128
+    display_step = 100
+
+    # Network Parameters
+    n_hidden_1 = 256 # 1st layer number of neurons
+    n_hidden_2 = 256 # 2nd layer number of neurons
+    num_input = 784 # MNIST data input (img shape: 28*28)
+    num_classes = 10 # MNIST total classes (0-9 digits)
+
+    # tf Graph input
+    X = tf.placeholder("float", [None, num_input])
+    Y = tf.placeholder("float", [None, num_classes])
+
+    # Store layers weight & bias
+    weights = {
+        'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([num_classes]))
+    }
+
+
+    # Create model
+    def neural_net(x):
+        # Hidden fully connected layer with 256 neurons
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        # Hidden fully connected layer with 256 neurons
+        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        # Output fully connected layer with a neuron for each class
+        out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+        return out_layer
+
+    # Construct model
+    logits = neural_net(X)
+    prediction = tf.nn.softmax(logits)
+
+    # Define loss and optimizer
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits, labels=Y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss_op)
+
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+
+    # Start training
 if sys.argv[3]=='2':
 
     mnist = tf.keras.datasets.mnist
@@ -73,10 +129,29 @@ if sys.argv[3]=='2':
 #========================================
 
 if sys.argv[3]=='1':
-    # Initialize Session and run `result`
-    with tf.Session() as sess:
-      output = sess.run(result)
-      print(output)
+     with tf.Session() as sess:
+
+        # Run the initializer
+        sess.run(init)
+
+        for step in range(1, num_steps+1):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop)
+            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+            if step % display_step == 0 or step == 1:
+                # Calculate batch loss and accuracy
+                loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+                                                                     Y: batch_y})
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.3f}".format(acc))
+
+        print("Optimization Finished!")
+
+        # Calculate accuracy for MNIST test images
+        print("Testing Accuracy:", \
+            sess.run(accuracy, feed_dict={X: mnist.test.images,
+                                          Y: mnist.test.labels}))
      
 elif sys.argv[3]=='2':
     print(result)
