0044b48920a587ac7371024e9eb5036ff2f1e2cf
Frederick Deny <fdeny@uoregon.edu>
2020-10-28 16:21:18 -0700

    Compiler changed to comply with project conventions

--- validation_tests/caliper/run.sh
+++ validation_tests/caliper/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+TMPFILE=$(mktemp ./tmp.XXXXXXX)
+
 . ./setup.sh
 
 eval ./caliper
 
-#CALI_CONFIG_PROFILE=runtime-report ./caliper
+CALI_CONFIG_PROFILE=runtime-report ./caliper
+
+if [ $? -ne 0 ]
+then
+  echo -e "${BRED}[FAILED]${NC}"
+  exit 1
+else
+  echo -e "${BGREEN}[PASSED]${NC}"
+fi
