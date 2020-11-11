b47b0c3aa0dd4efb2f626876f275e602bbc80d7e
wspear <wspear@cs.uoregon.edu>
2020-11-11 19:55:33 +0000

    Scr should copy the test case from the installed share directory.

--- validation_tests/scr/compile.sh
+++ validation_tests/scr/compile.sh
@@ -1,4 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 . ./setup.sh
-
+set -x
+mkdir -p build
+cd build
+cp  $SCR_ROOT/share/scr/examples/* .
 make  
+
+cd -
--- validation_tests/scr/run.sh
+++ validation_tests/scr/run.sh
@@ -1,14 +1,22 @@
-#!/bin/bash 
+#!/bin/bash -e
 #-ex
 . ./setup.sh
-export SCR_USER_NAME=root
+set -x
+cd ./build
+export SCR_USER_NAME=`whoami`
+#export SCR_PREFIX=/tmp/$SCR_USER_NAME/scr.defjobid
+CLEANTMP="rm -rf /tmp/$SCR_USER_NAME/scr.defjobid; rm -rf ./ckpt.*;rm -rf ./*.ckpt; rm -rf ./timestep.*"
+eval $CLEANTMP
 #mpirun -np 4 
-eval $TEST_RUN ./test_api  
+eval $TEST_RUN ./test_api 
+eval $CLEANTMP
 #mpirun -np 4 
 eval $TEST_RUN ./test_api_multiple  
+eval $CLEANTMP
 #mpirun -np 4 
 eval $TEST_RUN ./test_ckpt_C          
+eval $CLEANTMP
 #mpirun -np 4 
 eval $TEST_RUN ./test_ckpt_F
-
-
+eval $CLEANTMP
+cd -
