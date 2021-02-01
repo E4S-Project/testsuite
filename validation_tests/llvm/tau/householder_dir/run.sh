e5e15d14925bda3304efd2c4a84ab92268141698
Frederick Deny <fdeny@uoregon.edu>
2021-02-01 10:32:58 -0800

    updated setup file for mm_c_dir

--- validation_tests/llvm/tau/householder_dir/run.sh
+++ validation_tests/llvm/tau/householder_dir/run.sh
@@ -2,6 +2,6 @@
 
 . ./setup.sh
 
-#runtest "functions_C_files2.txt" "householder"
-#runtest "functions_C_files3.txt" "householderfileWC"
+runtest "functions_C_files2.txt" "householder"
+runtest "functions_C_files3.txt" "householderfileWC"
 runtest "functions_CXX_hh_files.txt" "householdercxx"
--- validation_tests/llvm/tau/mm_c_dir/setup.sh
+++ validation_tests/llvm/tau/mm_c_dir/setup.sh
@@ -1,7 +1,9 @@
-export LLVM=/home/users/fdeny/llvm_build/llvmVersions/llvminuse/llvm-project/llvm/install
-export PLUGIN_DIR=/home/users/fdeny/llvm_build/pluginVersions/plugin-tau-llvm-inuse/install/lib
-export TAU_INSTALL=/home/users/fdeny/llvm_build/tauVersions/tau2inuse/tau2/x86_64/lib/shared-TEST-clang
-module load gcc
+#!/bin/bash
 
 . ../testfunctions.sh
+
+SOURCES="matmult.c matmult_initialize.c"
+
+# Generate .symbol database if needed
+symbols::analysis "$SOURCES"
 
