010bd53a4b8cd348f46e633515f697e9cf70b9b5
Wyatt Spear <wspear@cs.uoregon.edu>
2021-05-12 16:22:02 -0700

    More text fixes

--- validation_tests/omega-h/compile.sh
+++ validation_tests/omega-h/compile.sh
@@ -2,4 +2,4 @@
 . ./setup.sh
 #mpicxx
 set -x
-eval $TEST_CXX_MPI -o castle main.cpp -I${OMEGA_H_ROOT}/include -L${OMEGA_H_LIB_DIR}  -lomega_h
+eval $TEST_CXX_MPI -o castle main.cpp -I${OMEGA_H_ROOT}/include -L${OMEGA_H_LIB_PATH}  -lomega_h
