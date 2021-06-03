b7755b9b618dd214286b83faaf034e3b879b3768
Wyatt Spear <wspear@cs.uoregon.edu>
2021-06-03 16:04:59 -0700

    All tests now need a setup.sh to allow single-setup to work.

--- validation_tests/mpi4py/run.sh
+++ validation_tests/mpi4py/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 #oneSpackHash(){ echo "/`spack find -l $1 | tail -n1 | awk '{print $1;}'`" ;  }
 #PYNUMPY=`oneSpackHash py-numpy`
-. ../../setup.sh
+. ./setup.sh
 #spackLoadUnique py-numpy
-spackLoadUnique py-mpi4py
-spackLoadUnique py-numpy
+#spackLoadUnique py-mpi4py
+#spackLoadUnique py-numpy
 #PYNUMPY=`spackLoadUnique py-numpy`
 #spack activate py-mpi4py
 #spack activate py-numpy%gcc 
