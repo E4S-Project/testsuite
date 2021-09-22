5ea243f81430e69c775c49e0d89652b3b9d16283
Scott K Warren <scott@rice.edu>
2021-09-22 15:12:07 -0500

    Adapt to preconditions expected by E4S test suite: Spack is in the environment nd the package has already been built.

--- validation_tests/hpctoolkit/setup.sh
+++ validation_tests/hpctoolkit/setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 . ../../setup.sh
 
-SPACK_ROOT=`spack location -r`
-. ${SPACK_ROOT}/share/spack/setup-env.sh
-
-spack install hpctoolkit@develop
-spack load hpctoolkit@develop
-
+spackLoadUnique hpctoolkit
