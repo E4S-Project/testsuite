8a4b8b044cc4697f182be90dd6ed989b55dfcdbc
Pieter Ghysels <pghysels@lbl.gov>
2021-11-01 17:33:33 -0700

    disable GPU in strumpack test

--- validation_tests/strumpack/run.sh
+++ validation_tests/strumpack/run.sh
@@ -4,7 +4,7 @@
 
 export OMP_NUM_THREADS=4
 
-./testPoisson2d 100
+./testPoisson2d 100 --sp_disable_gpu
 
 #mpirun -n 4 
-$TEST_RUN ./testPoisson2dMPIDist 100
+$TEST_RUN ./testPoisson2dMPIDist 100 --sp_disable_gpu
