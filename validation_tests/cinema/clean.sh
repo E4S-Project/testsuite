baa82c1bdd9d95297891b216bb9363046eb0b5d3
David Rogers <dhr@lanl.gov>
2021-11-30 15:39:16 -0500

    updating scripts

---
+++ validation_tests/cinema/clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+. ./setup.sh
+
+#
+# clean up, if there is anything that might interfere
+#
+if [ -d $CINEMASCI_TEST_DIR ]; then
+    echo "removing existing test directory: $CINEMASCI_TEST_DIR"
+    rm -rf $CINEMASCI_TEST_DIR
+fi
