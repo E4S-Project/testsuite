9cad8b2c13cd3836a5715fce238295d7b52d3722
Wyatt Spear <wspear@cs.uoregon.edu>
2021-12-09 14:57:39 -0800

    Papyrus test now works with or without shared libraries and quits at the first build/run error

--- validation_tests/papyrus/compile.sh
+++ validation_tests/papyrus/compile.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e 
 
 . ./setup.sh
-export LIBPAPYRUS=${PAPYRUS_ROOT}/lib/libpapyruskv.a
+set -x
+LIBPAPYRUS="-L${PAPYRUS_LIB_PATH} -lpapyruskv"
+PAPA=${PAPYRUS_LIB_PATH}/libpapyruskv.a
+if [ -f "$PAPA" ];then
+	LIBPAPYRUS=$PAPA
+fi
 
 TESTLIST="01_open_close  02_put_get  03_barrier  04_delete  05_fence  06_signal  07_consistency  08_protect  09_cache  10_checkpoint  11_restart  12_free "
 # The last one, 13_upc, is using UPC.
@@ -9,6 +14,6 @@ TESTLIST="01_open_close  02_put_get  03_barrier  04_delete  05_fence  06_signal 
 for d in $TESTLIST; do 
     cd $d
     #mpicxx 
-    eval $TEST_CXX_MPI -I${PAPYRUS_ROOT}/include -g -o test${d} test${d}.c $LIBPAPYRUS -lpthread -lm
+    eval $TEST_CXX_MPI -I${PAPYRUS_ROOT}/include -g -o test${d} test${d}.c ${LIBPAPYRUS} -lpthread -lm
     cd ..
 done
--- validation_tests/papyrus/run.sh
+++ validation_tests/papyrus/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 . ./setup.sh
+set -e
+set -x
 TESTLIST="01_open_close  02_put_get  03_barrier  04_delete  05_fence  06_signal  07_consistency  08_protect  09_cache  10_checkpoint  11_restart  12_free "
 NP=4
 
@@ -8,14 +10,14 @@ for d in $TESTLIST; do
     cd $d
     echo -n "Papyrus test $d:   "
     #mpiexec -n 
-    eval $TEST_RUN_CMD $TEST_RUN_PROCFLAG $NP ./test${d} 2>&1 > toto
+    ${TEST_RUN_CMD} $TEST_RUN_PROCFLAG $NP ./test${d} #2>&1 > toto
     # test completion and success
-    ERROR=`grep "\[E\] " toto`
-    if [[ ! -z $ERROR ]] ; then
-	echo "[FAIL]"
-    else
-	echo "[PASS]"
-    fi
-    rm toto
+    #ERROR=`grep "\[E\] " toto`
+    #if [[ ! -z $ERROR ]] ; then
+#	echo "[FAIL]"
+#    else
+#	echo "[PASS]"
+#    fi
+#    rm toto
     cd ..
 done
