4b8d56bfe6309ae4450b0e0bc76fa10910dee5eb
Wyatt Spear <wspear@cs.uoregon.edu>
2021-12-10 08:39:55 -0800

    Differentiate cuda and vanilla trilinos tests. Note: zoltan run error.

--- validation_tests/trilinos/compile.sh
+++ validation_tests/trilinos/compile.sh
@@ -4,6 +4,13 @@ set -x
 
 mkdir -p build
 cd build
-cmake ..
+
+CUDADEF=""
+if [ $USECUDA -eq 1 ];then
+        echo Using Cuda
+        CUDADEF="-DENABLE_E4S_CUDA=True"
+fi
+
+cmake ${CUDADEF}  ..
 make
 cd -
--- validation_tests/trilinos/setup.sh
+++ validation_tests/trilinos/setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique trilinos+cuda cuda_arch=80
-spackLoadUnique cmake
+
+THISDIR=`basename "$PWD"`
+USECUDA=0
+if [  "$THISDIR" = "trilinos-cuda" ];then
+        spackLoadUnique trilinos+cuda  #cuda_arch=80
+	USECUDA=1
+#elif [  "$THISDIR" = "trilinos-rocm" ];then
+#        spackLoadUnique trilinos+rocm
+else
+	spackLoadUnique trilinos~cuda
+fi
+
+spackLoadUnique cmake@3.17.1:
