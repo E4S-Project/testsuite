9a41c7113d3775babf416c9b913e22d9f5e1176e
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-01-19 10:44:40 -0800

    tau: simplify load logic

--- validation_tests/tau/setup.sh
+++ validation_tests/tau/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique mpich
-spackLoadUniqueNoR tau
-#NoRootVars tau #@develop  #NoR version turns off the -r to spack load, so no dependencies are auto-loaded
-#retVal=$?
-#if [ $retVal -ne 0 ] ; then
-#  exit $retVal
-#fi
-
+spackLoadUnique tau
