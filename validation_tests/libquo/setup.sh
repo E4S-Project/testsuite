fe385ba4d2e46801241efd922eb1c006964b8aaa
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-01-19 10:40:00 -0800

    libquo: remove unused logic

--- validation_tests/libquo/setup.sh
+++ validation_tests/libquo/setup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 . ../../setup.sh
 spackLoadUnique libquo
-spackLoadUnique hwloc
-#spackLoadUnique mpich
-#spackLoadUnique hwloc
-#spack load hwloc
-#spack load libquo
-#spack load mpich
-#
-#export HWLOC_ROOT=/opt/spack/opt/spack/linux-ubuntu18.04-x86_64/clang-11.0.0/hwloc-2.1.0-grinjchncvcfylps6fkicqa5dwewdy7c/
-#export QUO_ROOT=/usr/local/
