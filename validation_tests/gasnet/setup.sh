11bf3858dcc2a52d7f89f9f2f13de129105cc4ce
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-01-20 07:27:19 -0800

    gasnet: needs pkg-config for compile.sh

--- validation_tests/gasnet/setup.sh
+++ validation_tests/gasnet/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 . ../../setup.sh
+
+if ! command -v pkg-config >/dev/null 2>&1 ; then
+  spackLoadUnique pkg-config
+fi
+
 spackLoadUnique gasnet
 
 export PKG_CONFIG_PATH=$GASNET_ROOT/lib/pkgconfig/:$PKG_CONFIG_PATH
