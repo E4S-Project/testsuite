d1e56905487f64b61694555cda41e71081bdf835
Wyatt Spear <wspear@cs.uoregon.edu>
2022-02-10 13:28:47 -0800

    Added rocm symlink/logic for umpire

--- validation_tests/umpire/setup.sh
+++ validation_tests/umpire/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique umpire
+
+THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "umpire-cuda" ];then
+        spackLoadUnique umpire+cuda
+elif [  "$THISDIR" = "umpire-rocm" ];then
+        spackLoadUnique "umpire+rocm $TEST_ROCM_ARCH"
+        #export ACCEL_DEFINES="-DCMAKE_CXX_COMPILER=hipcc -DCMAKE_C_COMPILER=hipc"
+else
+        spackLoadUnique umpire~cuda~rocm
+fi
+
