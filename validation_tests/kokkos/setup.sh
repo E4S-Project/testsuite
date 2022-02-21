d7b2027c3ad939d79c5aaea7ccfc388d98e5b797
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-02-21 08:40:29 -0800

    kokkos-sycl test: load kokkos+sycl

--- validation_tests/kokkos/setup.sh
+++ validation_tests/kokkos/setup.sh
@@ -11,6 +11,9 @@ if [  "$THISDIR" = "kokkos-cuda" ];then
 elif [  "$THISDIR" = "kokkos-rocm" ];then
         spackLoadUnique "kokkos+rocm $TEST_ROCM_ARCH"
 	export ACCEL_DEFINES="-DCMAKE_CXX_COMPILER=hipcc -DCMAKE_C_COMPILER=hipc"
+elif [  "$THISDIR" = "kokkos-sycl" ];then
+        spackLoadUnique "kokkos +sycl"
+	export ACCEL_DEFINES="-DCMAKE_CXX_COMPILER=dpcpp"
 else
-	spackLoadUnique kokkos~cuda~rocm
+	spackLoadUnique kokkos~cuda~rocm~sycl
 fi
