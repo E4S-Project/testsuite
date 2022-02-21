29e2b20c493b0c0a2272e5c4078ab4a0b993a273
Wyatt Spear <wspear@cs.uoregon.edu>
2022-02-21 10:27:23 -0800

    Swapped superlu-dist test to use spack smoke test.

--- validation_tests/superlu-dist/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `cat testdirs.txt`; do
-  (cd "${i}" && ./clean.sh)
-done
--- validation_tests/superlu-dist/run.sh
+++ validation_tests/superlu-dist/run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 . ./setup.sh
-#set -x
-cwd=`pwd`
-for i in `cat testdirs.txt`; do
-  cd "${cwd}"
-  cd "${i}" && . ./run.sh || exit 1
-done
+spackTestRun ${SUPERLU_DIST_HASH}
