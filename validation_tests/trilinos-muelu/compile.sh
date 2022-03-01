f7ddad032d44120ea84e90d1ddb5c1a0caebcca0
Wyatt Spear <wspear@cs.uoregon.edu>
2022-02-28 16:53:30 -0800

    Added initial trilinos-muelu test. Builds but has runtime error.

---
+++ validation_tests/trilinos-muelu/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e 
+. ./setup.sh
+set -x
+
+mkdir -p build
+cd build
+
+CUDADEF=""
+if [ $USECUDA -eq 1 ];then
+        echo Using Cuda
+        CUDADEF="-DENABLE_E4S_CUDA=True"
+fi
+
+cmake ${CUDADEF}  ..
+make
+cd -
