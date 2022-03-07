2e82f0cf4e2090cd43d80276081c704b62707e56
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2022-03-07 09:21:38 -0800

    use https for cdash submission

--- validation_tests/trilinos-tpetra/test.cmake
+++ validation_tests/trilinos-tpetra/test.cmake
@@ -2,7 +2,7 @@
 set(CTEST_PROJECT_NAME "Trilinos")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "cdash.e4s.io")
 set(CTEST_DROP_LOCATION "/submit.php?project=Trilinos")
 set(CTEST_DROP_SITE_CDASH TRUE)
