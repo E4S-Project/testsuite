4e3f42a8d9608b86d357dc72098755bf2e2350b0
Wyatt Spear <wspear@cs.uoregon.edu>
2022-07-01 15:10:11 -0700

    Added arborx cuda and rocm symlinks. Test variants not yet implemented.

--- validation_tests/arborx/setup.sh
+++ validation_tests/arborx/setup.sh
@@ -5,4 +5,12 @@
 if ! command -v cmake >/dev/null 2>&1 ; then
   spackLoadUnique cmake
 fi
-spackLoadUnique arborx~rocm~cuda
+
+THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "arborx-cuda" ];then
+        spackLoadUnique arborx+cuda
+elif [  "$THISDIR" = "arborx-rocm" ];then
+        spackLoadUnique "arborx+rocm $TEST_ROCM_ARCH"
+else
+        spackLoadUnique arborx~cuda~rocm
+fi
