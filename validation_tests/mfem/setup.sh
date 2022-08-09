a1c001149b3a52202afb9de6a95573115967489a
Wyatt Spear <wspear@cs.uoregon.edu>
2022-08-09 11:37:14 -0700

    Removed redundant setup lines

--- validation_tests/mfem/setup.sh
+++ validation_tests/mfem/setup.sh
@@ -1,13 +1,2 @@
 . ../../setup.sh
-#ONEMFEM=`spackLoadUnique mfem`
 spackLoadUnique mfem
-#spack load $ONEMFEM 
-#spack load /hxeu37v #HDF5
-#spack load /6xsnuqx #Trilinos
-#spack load /373gdxv #Petsc
-#spack load mpich
-#spack load parmetis
-#spack load `oneSpackHash superlu-dist`
-export MFEM_INSTALL_DIR=${MFEM_ROOT}  #`spack location --install-dir $ONEMFEM`
-export MFEM_ROOT=$MFEM_INSTALL_DIR
-#echo $MFEM_INSTALL_DIR
