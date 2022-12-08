c928f81dfc21dbddb96c812d25e0f54535ec1b04
Wyatt Spear <wspear@cs.uoregon.edu>
2022-12-08 14:33:14 -0800

    Include excit library

--- validation_tests/aml/compile.sh
+++ validation_tests/aml/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . ./setup.sh
-
-$TEST_CC -o hello-world hello-world.c -I${NUMACTL_ROOT}/include -I${AML_ROOT}/include -L${AML_ROOT}/lib  -laml
+set -x
+$TEST_CC -o hello-world hello-world.c -I${NUMACTL_ROOT}/include -I${AML_ROOT}/include -L${AML_ROOT}/lib  -laml -lexcit
