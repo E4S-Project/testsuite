45eccf1b60c6f1f9005a815c08e50ce96a872104
Wyatt Spear <wspear@x1921c5s2b0n0.hostmgmt.cm.americas.sgi.com>
2023-01-19 22:17:09 +0000

    Only trilinos with zoltan can run these tests

--- validation_tests/trilinos/setup.sh
+++ validation_tests/trilinos/setup.sh
@@ -4,12 +4,12 @@
 THISDIR=`basename "$PWD"`
 USECUDA=0
 if [  "$THISDIR" = "trilinos-cuda" ];then
-        spackLoadUnique trilinos+cuda  #cuda_arch=80
+        spackLoadUnique trilinos+cuda+zoltan  #cuda_arch=80
 	USECUDA=1
 #elif [  "$THISDIR" = "trilinos-rocm" ];then
 #        spackLoadUnique trilinos+rocm
 else
-	spackLoadUnique trilinos~cuda
+	spackLoadUnique trilinos~cuda+zoltan
 fi
 
 if ! command -v cmake >/dev/null 2>&1 ; then
