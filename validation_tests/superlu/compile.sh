1f214a5dfb2f396100bae21e0ccf92c509dcbe75
Wyatt Spear <wspear@uan-0001.head.cm.americas.sgi.com>
2023-02-22 20:22:39 +0000

    Support manually defined link arguments for blas

--- validation_tests/superlu/compile.sh
+++ validation_tests/superlu/compile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 . ./setup.sh
-#spack load superlu
-#spack load openblas threads=none
-#gcc
 set -x
-${TEST_CC} -g ./c_sample.c  -I${SUPERLU_ROOT}/include/ -L${SUPERLU_ROOT}/lib64 -L${SUPERLU_ROOT}/lib -L${OPENBLAS_ROOT}/lib  -lsuperlu -lopenblas -lm -o c_sample
+
+#Detect external BLAS link line and use if present, otherwise default to openblas
+if [ -z ${TEST_BLAS_LINK+x} ] 
+then BLAS_LINK="-L${OPENBLAS_ROOT}/lib -lopenblas" 
+else BLAS_LINK=${TEST_BLAS_LINK}
+fi
+
+${TEST_CC} -g -O2 ./c_sample.c  -I${SUPERLU_ROOT}/include/ -L${SUPERLU_ROOT}/lib64 -L${SUPERLU_ROOT}/lib ${BLAS_LINK} -lsuperlu -lm -o c_sample
