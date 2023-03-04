dc6de7f9a6484f93e3bd6d23da43d021088c83a9
Wyatt Spear <wspear@login2.crusher.olcf.ornl.gov>
2023-03-03 20:17:12 -0500

    Use amdclang compilers to build arborx-rocm

--- validation_tests/arborx/setup.sh
+++ validation_tests/arborx/setup.sh
@@ -5,12 +5,15 @@
 if ! command -v cmake >/dev/null 2>&1 ; then
   spackLoadUnique cmake
 fi
-
+export ARBORX_CC=$TEST_CC
+export ARBORX_CXX=$TEST_CXX
 THISDIR=`basename "$PWD"`
 if [  "$THISDIR" = "arborx-cuda" ];then
         spackLoadUnique arborx+cuda $TEST_CUDA_ARCH
 elif [  "$THISDIR" = "arborx-rocm" ];then
         spackLoadUnique "arborx+rocm $TEST_ROCM_ARCH"
+	export ARBORX_CC=amdclang
+	export ARBORX_CXX=amdclang++
 else
         spackLoadUnique arborx~cuda~rocm
 fi
