ca45dcfcb3e605e6db4ce3e05e76a73e5ecd0e0d
Wyatt Spear <wspear@cs.uoregon.edu>
2023-03-03 17:04:01 -0800

    Allow magama-cuda test to use settings-defined blas library

--- validation_tests/magma-cuda/compile.sh
+++ validation_tests/magma-cuda/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 . ./setup.sh
 set -x
+
+#Detect external BLAS link line and use if present, otherwise default to openblas
+if [ -z ${TEST_BLAS_LINK+x} ]
+then BLAS_LINK="-L${OPENBLAS_ROOT}/lib -lopenblas"
+else BLAS_LINK=${TEST_BLAS_LINK}
+fi
+
+
 VERBOSE=1 make c 
