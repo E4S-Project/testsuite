f411f810cf7aaff6c5b148ba88e45fe3ed4003cd
Wyatt Spear <ac.wspear@florentia01.ftm.alcf.anl.gov>
2023-03-14 20:14:59 +0000

    Add python includes to compile command

--- validation_tests/swig/compile.sh
+++ validation_tests/swig/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 . ./setup.sh
-
+set -x
 swig -python example.i
 
-$TEST_CC -fpic -c example_wrap.c example.c
+$TEST_CC -fpic -c example_wrap.c example.c `python-config --includes` 
 $TEST_CC -shared -o _example.so example_wrap.o example.o
