b4689f340b6124ebd1d206146b55541d94b2faf9
Wyatt Spear <wspear@crusher098.crusher.olcf.ornl.gov>
2023-03-31 18:07:10 -0400

    Fix umpire rocm test build

--- validation_tests/umpire/compile.sh
+++ validation_tests/umpire/compile.sh
@@ -2,7 +2,7 @@
 . ./setup.sh
 #spack load umpire 
 set -x
-${TEST_CXX} malloc.cxx  -I$CAMP_ROOT/include   -I$UMPIRE_ROOT/include/ -L$UMPIRE_ROOT/lib  -lumpire
+${TEST_CXX} malloc.cxx  -I$CAMP_ROOT/include   -I$UMPIRE_ROOT/include/ -L$UMPIRE_ROOT/lib ${UMPIRE_ARCH_ARGS}  -lumpire
 
 #nvcc malloc.cxx -I$CUDA_ROOT/include  -I$CAMP_ROOT/include   -I$UMPIRE_ROOT/include/  -L$UMPIRE_LIB_PATH  -lumpire   -L$CUDA_LIB_PATH -lcuda -lcudart -lcudadevrt 
 
--- validation_tests/umpire/setup.sh
+++ validation_tests/umpire/setup.sh
@@ -6,6 +6,7 @@ if [  "$THISDIR" = "umpire-cuda" ];then
         spackLoadUnique umpire+cuda
 elif [  "$THISDIR" = "umpire-rocm" ];then
         spackLoadUnique "umpire+rocm $TEST_ROCM_ARCH"
+	export UMPIRE_ARCH_ARGS="-D__HIP_PLATFORM_AMD__ -I${HIP_ROOT}/include"
         #export ACCEL_DEFINES="-DCMAKE_CXX_COMPILER=hipcc -DCMAKE_C_COMPILER=hipc"
 else
         spackLoadUnique umpire~cuda~rocm
