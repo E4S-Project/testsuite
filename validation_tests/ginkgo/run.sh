2fa3f575e483389ac333f0472ea7d0d7c98af411
Wyatt Spear <wspear@cs.uoregon.edu>
2023-03-31 11:26:44 -0700

    Update/fix ginkgo cuda test invocations

--- validation_tests/ginkgo/run.sh
+++ validation_tests/ginkgo/run.sh
@@ -10,7 +10,14 @@ do
    # if [ "$APP" = "nine-pt-stencil-solver" -o "$APP" = "poisson-solver" -o "$APP" = "three-pt-stencil-solver" ]; then
 #	ARG="4"
 #    fi
+
+    if [ "$APP" = "minimal-cuda-solver" ]; then
+    	cat data/A.mtx data/b.mtx data/x0.mtx | ./minimal-cuda-solver
+    elif [ "$APP" = "simple-solver" -a "$THISDIR" = "ginkgo-cuda" ]; then
+	./simple-solver cuda
+    else 
     time timeout 2m srun ./${APP} ${ARG}
+    fi
     cd ..
 done
 
