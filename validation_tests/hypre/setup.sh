bb5800cf35e011f22ac56d4614920f4e2c1d174c
Wyatt Spear <wspear@login13.frontier.olcf.ornl.gov>
2023-04-26 21:38:02 -0400

    Divided hypre into rocm/cuda/cpu tests (based on loaded variant only)

--- validation_tests/hypre/setup.sh
+++ validation_tests/hypre/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique hypre@2.27.0: 
+if [  "$THISDIR" = "hypre-cuda" ];then
+        spackLoadUnique "hypre@2.27.0:+cuda $TEST_CUDA_ARCH"
+elif [  "$THISDIR" = "hypre-rocm" ];then
+        spackLoadUnique "hypre@2.27.0:+rocm $TEST_ROCM_ARCH"
+else
+        spackLoadUnique hypre@2.27.0:~cuda~rocm
+fi
