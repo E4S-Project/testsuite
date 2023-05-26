dd7dbaebf7eade9250a7635e29e2794d5bc1b181
Wyatt Spear <wspear@cs.uoregon.edu>
2023-05-26 10:22:22 -0700

    aml needs pthread

--- validation_tests/aml/compile.sh
+++ validation_tests/aml/compile.sh
@@ -2,4 +2,4 @@
 
 . ./setup.sh
 set -x
-$TEST_CC -o hello-world hello-world.c -I${NUMACTL_ROOT}/include -I${AML_ROOT}/include -L${AML_ROOT}/lib  -laml -lexcit
+$TEST_CC -o hello-world hello-world.c -I${NUMACTL_ROOT}/include -I${AML_ROOT}/include -L${AML_ROOT}/lib  -laml -lexcit -pthread
