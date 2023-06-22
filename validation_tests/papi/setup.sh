2ba5d2c2b7cd22960349057f2397cbc39c3c7579
Wyatt Spear <wspear@cs.uoregon.edu>
2023-06-22 13:43:44 -0700

    Don't commit binaries!

--- validation_tests/papi/setup.sh
+++ validation_tests/papi/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique papi@master #>>>PACKAGE<<<
-#export E4S_TEST_SOURCE=>>>SOURCEFILE.EXT<<<
-#export E4S_TEST_FLAGS=>>>FLAGS<<<
-#export E4S_TEST_LIBS=>>>LIBS<<<
+spackLoadUnique papi
