1f6539be7756ef944cc6c83b039cb68550db8562
Wyatt Spear <wspear@login08.frontier.olcf.ornl.gov>
2023-10-20 19:28:08 -0400

    Build raja-rocm test with hipcc

--- validation_tests/raja/compile.sh
+++ validation_tests/raja/compile.sh
@@ -13,7 +13,7 @@ if [ $USECUDA -eq 1 ];then
 	CUDADEF="-DENABLE_CUDA=True"
 elif [ $USEROCM -eq 1 ];then
         echo Using Rocm
-        ROCMDEF="-DENABLE_HIP=True"
+        ROCMDEF="-DCMAKE_C_COMPILER=amdclang -DCMAKE_CXX_COMPILER=hipcc  -DENABLE_HIP=True"
 fi
 
 cmake ${ROCMDEF}  ${CUDADEF} -DBLT_SOURCE_DIR=${BLT_ROOT} -DRAJA_DIR=${RAJA_ROOT} ..
