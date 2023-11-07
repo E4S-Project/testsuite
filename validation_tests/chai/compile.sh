a9d199086b719cc633c8e226e15c9dbe02709948
Wyatt Spear <wspear@cs.uoregon.edu>
2023-11-06 16:27:30 -0800

    Added rocm test for chai

--- validation_tests/chai/compile.sh
+++ validation_tests/chai/compile.sh
@@ -4,5 +4,15 @@ set -x
 set -e
 mkdir -p build
 cd build
-cmake .. -DBLT_SOURCE_DIR=$BLT_ROOT -Dcamp_DIR=$CAMP_ROOT -Dumpire_DIR=$UMPIRE_ROOT -Dchai_DIR=$CHAI_ROOT
+
+if [ $USECUDA -eq 1 ];then
+        echo Using Cuda
+        CUDADEF="-DENABLE_CUDA=True"
+elif [ $USEROCM -eq 1 ];then
+        echo Using Rocm
+        ROCMDEF="-DCMAKE_C_COMPILER=amdclang -DCMAKE_CXX_COMPILER=hipcc  -DENABLE_HIP=True"
+fi
+
+
+cmake .. ${ROCMDEF}  -DBLT_SOURCE_DIR=$BLT_ROOT -Dcamp_DIR=$CAMP_ROOT -Dumpire_DIR=$UMPIRE_ROOT -Dchai_DIR=$CHAI_ROOT
 make
--- validation_tests/chai/setup.sh
+++ validation_tests/chai/setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique chai 
+
+THISDIR=`basename "$PWD"`
+USECUDA=0
+USEROCM=0
+if [  "$THISDIR" = "chai-cuda" ];then
+        spackLoadUnique chai+cuda
+        USECUDA=1
+elif [  "$THISDIR" = "chai-rocm" ];then
+        spackLoadUnique chai+rocm
+        USEROCM=1
+else
+        spackLoadUnique chai~cuda~rocm
+fi
