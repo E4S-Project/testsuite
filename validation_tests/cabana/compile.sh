031fd5669fc0650d59b45a660d62c4effbb5d714
Wyatt Spear <wspear@cs.uoregon.edu>
2023-11-06 16:04:40 -0800

    Add rocm test for cabana

--- validation_tests/cabana/compile.sh
+++ validation_tests/cabana/compile.sh
@@ -2,9 +2,19 @@
 
 . ./setup.sh
 
-cmake \
-	-DCMAKE_C_COMPILER=$TEST_CC \
-	-DCMAKE_CXX_COMPILER=$TEST_CXX \
+COMPDEF="-DCMAKE_C_COMPILER=$TEST_CC  -DCMAKE_CXX_COMPILER=$TEST_CXX"
+
+
+if [ $USECUDA -eq 1 ];then
+        echo Using Cuda
+        CUDADEF="-DENABLE_CUDA=True"
+elif [ $USEROCM -eq 1 ];then
+        echo Using Rocm
+        COMPDEF="-DCMAKE_C_COMPILER=amdclang -DCMAKE_CXX_COMPILER=hipcc  -DENABLE_HIP=True"
+fi
+
+
+cmake $COMPDEF \
 	-DCabana_ROOT=Cabana_ROOT .
 
 make
--- validation_tests/cabana/setup.sh
+++ validation_tests/cabana/setup.sh
@@ -3,4 +3,16 @@
 . ../../setup.sh
 
 spackLoadUnique cmake
-spackLoadUnique cabana
+
+THISDIR=`basename "$PWD"`
+USECUDA=0
+USEROCM=0
+if [  "$THISDIR" = "cabana-cuda" ];then
+        spackLoadUnique raja+cuda
+        USECUDA=1
+elif [  "$THISDIR" = "cabana-rocm" ];then
+        spackLoadUnique cabana+rocm
+        USEROCM=1
+else
+        spackLoadUnique cabana~cuda~rocm
+fi
