9c4787f6b83de756fcb66537db088b2a23865098
AWS ParallelCluster user <ec2-user@ip-10-0-0-200.us-west-2.compute.internal>
2024-03-15 22:44:39 +0000

    Update kokkos test program path

--- validation_tests/kokkos/run.sh
+++ validation_tests/kokkos/run.sh
@@ -4,5 +4,6 @@ set -x
 export OMP_PROC_BIND=spread 
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=8
-./build/example_with_kokkos 500000000
-./build/example_cmake 500000000
+./build_cmake_installed/build/example 500000000
+#./build/example_with_kokkos 500000000
+#./build/example_cmake 500000000
