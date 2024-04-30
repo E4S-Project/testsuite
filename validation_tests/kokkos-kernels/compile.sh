d6968ba0a38ab7f4022c082a1ca467434fa12859
Wyatt Spear <wspear@cs.uoregon.edu>
2024-04-30 21:59:59 +0000

    Update compile

--- validation_tests/kokkos-kernels/compile.sh
+++ validation_tests/kokkos-kernels/compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 . ./setup.sh
 set -x
-#g++
-eval $TEST_CXX -fopenmp -c blockjacobi.cpp -I${KOKKOS_ROOT}/include -I${KOKKOS_KERNELS_ROOT}/include
-#g++  
-eval $TEST_CXX -fopenmp blockjacobi.o -o blockjacobi -L${KOKKOS_LIB_PATH} -L${KOKKOS_KERNELS_LIB_PATH} -lkokkoskernels -lkokkoscore
-
+mkdir install/build
+cd install/build
+cmake ..
+make
