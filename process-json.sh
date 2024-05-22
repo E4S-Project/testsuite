ab17337c0135f11239c4832f353bc18af5f8f047
Wyatt Spear <wspear@cs.uoregon.edu>
2024-05-22 11:35:54 -0700

    Added csv output to json processor

--- process-json.sh
+++ process-json.sh
@@ -5,6 +5,7 @@ print_usage() {
 	echo "Usage: $0 [options] <json_file> [key (setup, clean, compile, run{default})] [value (pass{default}, fail, missing)]"
   echo "Options:"
   echo "  -l  Print output on a single line"
+  echo "  -c  Print output in csv format (ignore filters)" 
   echo "  -h  Print this help message"
 }
 
@@ -37,6 +38,16 @@ parse_json() {
   echo # Print a new line after the output
 }
 
+to_csv(){
+local json_file="$1"
+echo $json_file
+jq -r 'map(select(.test != null)) | .[] | [.test, (.test_stages | to_entries[-1].value), if (.test_stages | to_entries[-1].value) == "fail" then (.test_stages | to_entries[-1].key) else "" end] | @csv' $json_file | tr -d '"' | tr -d '/'
+
+#csv=`jq -r 'map(select(.test != null)) | .[] | [.test, (.test_stages | to_entries[-1].value), if (.test_stages | to_entries[-1].value) == "fail" then (.test_stages | to_entries[-1].key) else "" end] | @csv' $json_file`
+#echo $csv
+}
+
+
 # Check if -h option is passed
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
   print_usage
@@ -50,6 +61,15 @@ if [[ -z "$1" ]]; then
 fi
 
 # Check if -l option is passed
+if [[ "$1" == "-c" ]]; then
+  if [[ -n "$2" ]]; then
+    to_csv "$2"
+  else
+    print_usage
+    exit 1
+  fi
+  exit 0
+fi
 if [[ "$1" == "-l" ]]; then
   if [[ -n "$2" ]]; then
     parse_json "$2" "-l" "${3:-run}" "${4:-pass}"
