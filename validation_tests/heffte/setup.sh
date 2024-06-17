73edfd01714efff90078871152bd4ff36616611b
Wyatt Spear <wspear@cs.uoregon.edu>
2024-06-17 23:54:44 +0000

    Add cuda, fftw and rocm specific tests for heffte

--- validation_tests/heffte/setup.sh
+++ validation_tests/heffte/setup.sh
@@ -2,4 +2,16 @@
 
 . ../../setup.sh
 
-spackLoadUnique heffte@2.2.0: +fftw
+TESTNAME=heffte
+TESTVERSION=@2.2.0:
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-fftw" ];then
+	spackLoadUnique "$TESTNAME+fftw $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm~fftw $TESTVERSION
+fi
+
