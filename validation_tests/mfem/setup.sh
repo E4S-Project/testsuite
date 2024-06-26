5fadd4b3f9726286d205b0adb13921bde309b0ab
Wyatt Spear <wspear@cs.uoregon.edu>
2024-06-26 23:17:57 +0000

    Extended mfem example to rocm/cuda

--- validation_tests/mfem/setup.sh
+++ validation_tests/mfem/setup.sh
@@ -1,2 +1,12 @@
 . ../../setup.sh
-spackLoadUnique mfem~cuda~rocm
+TESTNAME=mfem
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+fi
+
