c1a8f30b5298c4502f243d8008b318cab623cf17
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-01 23:33:13 +0000

    Added slate ~rocm~cuda test

--- validation_tests/slate-cuda/compile.sh
+++ validation_tests/slate-cuda/compile.sh
@@ -10,11 +10,12 @@ then BLAS_LINK="-L${OPENBLAS_ROOT}/lib -lopenblas"
 else BLAS_LINK=${TEST_BLAS_LINK}
 fi
 
+if [ -n "$SLATE_CUDA_TEST" ]; then
 #Detect external CUDA link line and use if present, otherwise default 
 if [ -z ${TEST_CUDA_LINK+x} ]
 then CUDA_LINK="-L${CUDA_LIB_PATH} -lcudart -lcublas -lcusolver"
 else CUDA_LINK=${TEST_CUDA_LINK}
 fi
-
+fi
 
 ${TEST_CXX_MPI} -o slate04_blas slate04_blas.cc -I${BLASPP_ROOT}/include -I${CUDA_ROOT}/include  -I${LAPACKPP_ROOT}/include  -I${SLATE_ROOT}/include -L${BLASPP_LIB_PATH}  -L${SLATE_LIB_PATH}  -lblaspp -lslate  ${BLAS_LINK}  ${CUDA_LINK} -fopenmp
--- validation_tests/slate-cuda/setup.sh
+++ validation_tests/slate-cuda/setup.sh
@@ -2,4 +2,17 @@
 
 . ../../setup.sh
 
-spackLoadUnique slate+cuda
+TESTNAME=slate
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+	export SLATE_CUDA_TEST=1
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+	export SLATE_ROCM_TEST=1
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+	export SLATE_NOACC_TEST=1
+fi
+
