c71a5e816704585b9f7cd28f7726cab62124a855
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-15 20:33:55 +0000

    Added kokkos-kernels cuda test

--- validation_tests/kokkos-kernels/setup.sh
+++ validation_tests/kokkos-kernels/setup.sh
@@ -1,7 +1,18 @@
 . ../../setup.sh
 
+TESTNAME=kokkos-kernels
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME" ];then
+        spackLoadUnique "$TESTNAME+openmp $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_ROCM_ARCH $TESTVERSION"
+fi
+#else
+#        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+#fi
 #spackLoadUniqueNoR kokkos+openmp
-spackLoadUniqueNoR kokkos-kernels+openmp
+#spackLoadUniqueNoR kokkos-kernels+openmp
 #kokkosHash=$(spackLoadUniqueNoR kokkos+openmp)
 #kokkoskernelsHash=$(spackLoadUniqueNoR kokkos-kernels+openmp)
 #export KOKKOS=KOKKOS_ROOT #`spack location -i ${kokkosHash}`
