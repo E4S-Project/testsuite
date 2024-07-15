8e0856d4467a52d781c2aa86588829ec09184c0f
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-15 18:52:49 +0000

    Added strumpack accelerated tests

--- validation_tests/strumpack/run.sh
+++ validation_tests/strumpack/run.sh
@@ -4,7 +4,7 @@
 set -x
 export OMP_NUM_THREADS=1
 
-./testPoisson2d 100 --sp_disable_gpu
+./testPoisson2d 100 $runArg 
 
 #mpirun -n 4 
-$TEST_RUN ./testPoisson2dMPIDist 100 --sp_disable_gpu
+$TEST_RUN ./testPoisson2dMPIDist 100 $runArg 
--- validation_tests/strumpack/setup.sh
+++ validation_tests/strumpack/setup.sh
@@ -6,4 +6,17 @@ if ! command -v cmake >/dev/null 2>&1 ; then
   spackLoadUniqueNoX cmake
 fi
 
-spackLoadUnique strumpack~rocm
+TESTNAME=strumpack
+TESTVERSION=
+export runArg=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+	export runArg="--sp_disable_gpu"
+fi
+
+
