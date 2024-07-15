f91d7b0ad1ec661090b09f70c677c52717cebf9f
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-15 19:23:43 +0000

    Added accelerated tests for slepc

--- validation_tests/slepc/setup.sh
+++ validation_tests/slepc/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 . ../../setup.sh
-spackLoadUnique slepc
+TESTNAME=slepc
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+fi
 export SLEPC_DIR=$SLEPC_ROOT
 export PETSC_DIR=$PETSC_ROOT
