242eaf79494c4c5cbd818bfa4d7859116f651b84
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-17 23:22:46 +0000

    Added cuda test for lammps

--- validation_tests/lammps/setup.sh
+++ validation_tests/lammps/setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 . ../../setup.sh
 THISDIR=`basename "$PWD"`
-spackLoadUnique $THISDIR
-export E4S_SPACK_TEST_HASH=`spackHashFromName $THISDIR`
+
+TESTNAME=lammps
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+	#export E4S_SPACK_TEST_HASH=`spackHashFromName $TESTNAME+cuda`
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+	#export E4S_SPACK_TEST_HASH=`spackHashFromName $TESTNAME+rocm`
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+	#export E4S_SPACK_TEST_HASH=`spackHashFromName $TESTNAME`
+fi
 
