d7f343b6c6a3a5f3824b8c785c261849968d616e
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-18 23:44:32 +0000

    Added cuda and rocm tests for legion

--- validation_tests/legion/setup.sh
+++ validation_tests/legion/setup.sh
@@ -3,4 +3,15 @@
 . ../../setup.sh
 
 spackLoadUniqueNoX cmake
-spackLoadUnique legion
+
+TESTNAME=legion
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda~rocm $TESTVERSION
+fi
+
