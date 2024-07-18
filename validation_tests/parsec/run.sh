c72adb83edf5b413dea8161d18d41a0832bfe604
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-18 20:46:05 +0000

    Added parsec cuda test

--- validation_tests/parsec/run.sh
+++ validation_tests/parsec/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 . ./setup.sh
-if [ $e4s_print_color = true -a  -n "$TERM" ];
-then
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
+#if [ $e4s_print_color = true -a  -n "$TERM" ];
+#then
+#RED='\033[0;31m'
+#GREEN='\033[0;32m'
+#BLUE='\033[0;34m'
 
-BRED='\033[1;31m'
-BGREEN='\033[1;32m'
-BBLUE='\033[1;34m'
+#BRED='\033[1;31m'
+#BGREEN='\033[1;32m'
+#BBLUE='\033[1;34m'
 
-NC='\033[0m'
-fi
+#NC='\033[0m'
+#fi
 ret=0
 for PROGNAME in dtd_test_allreduce write_check; do
     echo -e "${BBLUE}### TESTING${NC} $PROGNAME"
--- validation_tests/parsec/setup.sh
+++ validation_tests/parsec/setup.sh
@@ -6,6 +6,16 @@ if ! command -v cmake >/dev/null 2>&1 ; then
   spackLoadUniqueNoX cmake
 fi
 
-spackLoadUnique parsec
+TESTNAME=parsec
+TESTVERSION=
+export THISDIR=`basename "$PWD"`
+if [  "$THISDIR" = "$TESTNAME-cuda" ];then
+        spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
+#elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+#        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+else
+        spackLoadUnique $TESTNAME ~cuda $TESTVERSION
+fi
+
 
 NP=4
