3243bff506a76330cf9d733108bc392b594680d3
Wyatt Spear <wspear@cs.uoregon.edu>
2024-07-18 22:47:48 +0000

    Added exago rocm test

--- validation_tests/exago/setup.sh
+++ validation_tests/exago/setup.sh
@@ -7,8 +7,8 @@ TESTVERSION=
 export THISDIR=`basename "$PWD"`
 if [  "$THISDIR" = "$TESTNAME-cuda" ];then
         spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH $TESTVERSION"
-#elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
-#        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
+elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
+        spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH $TESTVERSION"
 else
         spackLoadUnique $TESTNAME ~cuda $TESTVERSION
 fi
