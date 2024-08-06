814b254097af302816d4ee29dd58ffd566cf784a
Wyatt Spear <wspear@cs.uoregon.edu>
2024-08-06 19:21:07 +0000

    Added nco test

---
+++ validation_tests/nco/README.sh
@@ -0,0 +1,1 @@
+Test data from: https://www.unidata.ucar.edu/software/netcdf/examples/files.html
--- validation_tests/nrm/compile.sh
+++ validation_tests/nrm/compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -e
-echo "Internal Spack test. No build step required."
+. ./setup.sh
+set -x
+#echo "Internal Spack test. No build step required."
+${TEST_CC} ./test.c -I$NRM_ROOT/include -L$NRM_LIB_PATH -lnrm
