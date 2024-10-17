f5a5037ddd3c04425178a663922c71bb62180947
Dax <Declanxlynch@gmail.com>
2024-10-17 06:39:20 +0000

    Got output updating at the correct time. Need to write the printer function

--- test2-all.py
+++ test2-all.py
@@ -30,15 +30,15 @@ import time
 # Function to execute shell scripts and handle errors, asynchronously by the worker queue
 def async_worker(queue, timeout=False, print_json=False, timestamp=""):
     # Change to the specified directory and update environment variables
+    os.environ['testtime'] = timestamp
 
     while (queue.qsize() > 0): # here
         worker_pipe = queue.get() #here
         current_dir_with_symlinks = worker_pipe.recv()
-        worker_pipe.send(current_dir_with_symlinks)
+        worker_pipe.send(current_dir_with_symlinks + "\n")
         os.chdir(current_dir_with_symlinks)
         os.environ['PWD']=current_dir_with_symlinks #chdir doesn't change this and scripts such as setup.sh/compile.sh require this to be set
         os.environ['testdir'] = current_dir_with_symlinks
-        os.environ['testtime'] = timestamp
 
         log_suffix = f"{os.path.basename(current_dir_with_symlinks)}_{timestamp}.log"
 
@@ -57,24 +57,43 @@ def async_worker(queue, timeout=False, print_json=False, timestamp=""):
         ]
         
         # Filter out any empty stages
-        command = ' && '.join([stage for stage in stages if stage])
+        command =  ' && '.join([stage for stage in stages if stage])
         
 
         
         result = subprocess.Popen(
             command, 
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        #    stdout=worker_pipe,
-         #   stderr=worker_pipe,
+            stderr=subprocess.DEVNULL,
             shell=True,
-            text=True
+            text=True,
         )
-        
-        stdout, stderr = result.communicate()
-        worker_pipe.send(stdout)
+        while result.poll() == None:
+            worker_pipe.send(result.stdout.readline())
+        worker_pipe.send(result.stdout.read())
+        worker_pipe.send("-----Return Code-----="+str(result.returncode)+"\n")
         worker_pipe.close()
     return -1
+
+def print_results(results):
+    for r in results: #If r is a string, print it, if r is not a string, it represents a 
+                      #results from an asynchronous job call, and thus process its output
+
+        if type(r) != type(""):
+            while True:
+                ready = wait([r], .1) #Block only temporarily 
+                if ready: #Pipe is closed or a message is ready or both
+                    try:
+                        msg = r.recv() #This should complete if the pipe has any data
+                        print(msg, end="")
+                    except EOFError: #If the pipe has no data and is closed
+            #            print("Pipe has been closed")
+                        break
+                else:
+                    pass
+
+        else:
+            print(r)
 
 # Function to iterate through directories in a non-recursive manner, adding calls to iterate_files.sh to a worker queue.
 #Given a directory, this finds all sub directories that contain a run.sh, and then the worker processes go through each 
@@ -150,29 +169,10 @@ def iterate_directories(testdir, processes=4, scheduler=False, scheduler_flags="
         Processes[i].start()
     
 
-    for r in results: #If r is a string, print it, if r is not a string, it represents a 
-                      #results from an asynchronous job call, and thus process its output
-
-        if type(r) != type(""):
-            while True:
-                ready = wait([r], .1) #Block only temporarily 
-                if ready: #Pipe is closed or a message is ready or both
-                    try:
-                        msg = r.recv() #This should complete if the pipe has any data
-                        print(msg)
-                    except EOFError: #If the pipe has no data and is closed
-                        print("Pipe has been closed")
-                        break
-                else:
-                    pass
-
-        else:
-            print(r)
+    print_results(results)
     # End JSON output if needed
-    print("JOininh")
     for process in Processes:
         process.join()
-    print("Joined")
 
     if print_json:
         with open(json_output_file,"a+") as file:
