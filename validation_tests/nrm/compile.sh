e9ed3304d04ef359ec7b35d4ed68f6f53a04d278
Wyatt Spear <wspear@cs.uoregon.edu>
2025-02-21 14:51:59 -0800

    Superfluous compile command

--- validation_tests/nrm/compile.sh
+++ validation_tests/nrm/compile.sh
@@ -2,4 +2,4 @@
 . ./setup.sh
 set -x
 #echo "Internal Spack test. No build step required."
-${TEST_CC} ./test.c -I$NRM_ROOT/include -L$NRM_LIB_PATH -lnrm
+#${TEST_CC} ./test.c -I$NRM_ROOT/include -L$NRM_LIB_PATH -lnrm
