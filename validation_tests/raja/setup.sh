346e21fb5f5c371f240ffd4848746d57dd918261
Wyatt Joel Spear <wspear@hopper1.nic.uoregon.edu>
2026-06-24 16:18:10 -0700

    raja-cuda: fix build

--- validation_tests/raja/setup.sh
+++ validation_tests/raja/setup.sh
@@ -8,6 +8,7 @@ USECUDA=0
 USEROCM=0
 if [  "$THISDIR" = "raja-cuda" ];then
 	spackLoadUnique raja+cuda
+	export SPACK_CUDA_ARCH=$(spack find --json /$RAJA_HASH | python3 -c 'import sys,json; print(json.load(sys.stdin)[0]["parameters"]["cuda_arch"][0])')
 	USECUDA=1
 elif [  "$THISDIR" = "raja-rocm" ];then
 	spackLoadUnique raja+rocm
