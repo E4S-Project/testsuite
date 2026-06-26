68c2610127d6baaa97588184719c33573c8ef47e
Wyatt Joel Spear <wspear@hopper1.nic.uoregon.edu>
2026-06-26 11:48:37 -0700

    axom-cuda: fix build. update cmake target

--- validation_tests/axom/setup.sh
+++ validation_tests/axom/setup.sh
@@ -5,9 +5,13 @@ TESTNAME=axom
 export THISDIR=`basename "$PWD"`
 if [  "$THISDIR" = "$TESTNAME-cuda" ];then
         spackLoadUnique "$TESTNAME+cuda $TEST_CUDA_ARCH"
+	export SPACK_CUDA_ARCH=$(spack find --json /$RAJA_HASH | python3 -c 'import sys,json; print(json.load(sys.stdin)[0]["parameters"]["cuda_arch"][0])')
+	export TEST_COMPILER_BACKEND="CUDA"
 elif [  "$THISDIR" = "$TESTNAME-rocm" ];then
         spackLoadUnique "$TESTNAME+rocm $TEST_ROCM_ARCH"
+	export TEST_COMPILER_BACKEND="HIP"
 else
         spackLoadUnique $TESTNAME~cuda~rocm
+	export TEST_COMPILER_BACKEND="HOST"
 fi
 
