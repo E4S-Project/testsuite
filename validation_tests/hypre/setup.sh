281e74ebc02c70c10768b886e1e5d84ec0b8ad86
Wyatt Spear <wspear@cs.uoregon.edu>
2026-09-18 13:42:59 -0700

    hypre-rocm: add rocm lib paths from spack

--- validation_tests/hypre/setup.sh
+++ validation_tests/hypre/setup.sh
@@ -9,7 +9,11 @@ if [  "$THISDIR" = "hypre-cuda" ];then
 	MPIRANKS=1
 elif [  "$THISDIR" = "hypre-rocm" ];then
         spackLoadUnique "hypre@2.27.0:+rocm $TEST_ROCM_ARCH"
-	ARCHARGS="-lrocsparse -lrocblas -lamdhip64"
+	#ARCHARGS="-lrocsparse -lrocblas -lamdhip64"
+	ARCHARGS="-L${ROCSPARSE_LIB_PATH} -Wl,-rpath,${ROCSPARSE_LIB_PATH} \
+                  -L${ROCBLAS_LIB_PATH} -Wl,-rpath,${ROCBLAS_LIB_PATH} \
+                  -L${HIP_LIB_PATH} -Wl,-rpath,${HIP_LIB_PATH} \
+                  -lrocsparse -lrocblas -lamdhip64"
 	MPIRANKS=1
 else
         spackLoadUnique hypre@2.27.0:~cuda~rocm
