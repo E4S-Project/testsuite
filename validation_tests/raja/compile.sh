8b0ade16e8c5dad570f1e0368e75660503098048
Wyatt Spear <wspear@uoregon.edu>
2026-09-18 18:15:13 -0700

    raja-cuda: fix cuda arch var

--- validation_tests/raja/compile.sh
+++ validation_tests/raja/compile.sh
@@ -1,19 +1,21 @@
 #!/bin/bash  
 . ./setup.sh
-spackLoadUniqueNoX cmake
+#spackLoadUniqueNoX cmake
 CUDADEF=""
 ROCMDEF=""
 set -e
 set -x
+
+echo $SPACK_CUDA_ARCH
 mkdir build
 cd build
 if [ $USECUDA -eq 1 ];then
 	echo Using Cuda
-	CUDADEF="-DENABLE_CUDA=True"
+	CUDADEF="-DENABLE_CUDA=True -DCMAKE_CUDA_ARCHITECTURES=${SPACK_CUDA_ARCH}"
 elif [ $USEROCM -eq 1 ];then
         echo Using Rocm
         ROCMDEF="-DCMAKE_C_COMPILER=amdclang -DCMAKE_CXX_COMPILER=hipcc  -DENABLE_HIP=True"
 fi
 
-cmake ${ROCMDEF}  ${CUDADEF}  -DRAJA_DIR=${RAJA_ROOT} ..   #-DBLT_SOURCE_DIR=${BLT_ROOT}
+cmake ${ROCMDEF}  ${CUDADEF}  -DRAJA_DIR=${RAJA_ROOT} ..
 make
